b78c1da3aebc4ed043678612ba42632074b6fa3d
Juan Orozco <juanthedesigner@gmail.com>
2015-02-01 20:51:33 -0500

    Commiting better docjs config.

---
+++ test.js
@@ -0,0 +1,15 @@
+/**
+* @module {function} docjs-sanbox/heyo Heyo
+* @parent docjs-sanbox
+* @author Juan Orozco
+*
+* @description Aren't we all?
+*
+* @signature
+* heyo();
+*
+*/
+
+exports.heyo = function(){
+    return "IMA LEGO";
+}
